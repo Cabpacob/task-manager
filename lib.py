289efa16f0d22921078413ad74d7c629ad482bf9
Cabpacob <Cabpacob@github.com>
2021-09-26 20:20:13 +0300

    Implemented class Task

--- lib.py
+++ lib.py
@@ -6,16 +6,27 @@ class TaskManager:
     __tasks = []
 
     def add_task(self, task_name, progress, spent_hours=None):
-        self.__tasks.append((task_name.strip(), progress, spent_hours, time.time()))
+        class Task:
+            def __init__(self, name, progress, spent_hours, current_time):
+                assert progress >= 0 and progress <= 1, 'Wrong progress, it must be betwen 0 and 1'
+                assert spent_hours is None or data.spent_hours > 0, 'Wrong spent_hours, it must be greater than 0'
+
+                self.name = name.strip()
+                self.progress = progress
+                self.spent_hours = spent_hours
+                self.current_time = current_time
+
+            def __str__(self):
+                result = f'You did task {self.name} by {int(self.progress * 100)}% at {time.ctime(self.current_time)}'
+                if task[2] is not None:
+                    result += f' and spent {self.spent_hours} hours'
+                return result
+
+
+        self.__tasks.append(Task(task_name, progress, spent_hours, time.time()))
 
     def get_work(self):
-        def task_to_string(task):
-            result = f'You did task {task[0]} by {int(task[1] * 100)}% at {time.ctime(task[3])}'
-            if task[2] is not None:
-                result += f' and spent {task[2]} hours'
-            return result
-
-        return '\n'.join(map(task_to_string, self.__tasks))
+        return '\n'.join(map(str, self.__tasks))
 
 
 class TaskProgress(BaseModel):
