83e0e28b1ea2443e48844f45f1bafceb11c01ba7
Cabpacob <Cabpacob@github.com>
2021-09-26 20:21:41 +0300

    Implemented exception handling from Task

--- main.py
+++ main.py
@@ -21,11 +21,8 @@ def get_tasks():
 
 @app.post('/register-task')
 def get_task_progress(data: TaskProgress):
-    if data.progress < 0 or data.progress > 1:
-        return Response('Wrong progress, it must be betwen 0 and 1', status_code=status.HTTP_400_BAD_REQUEST)
-
-    if data.spent_hours is not None and data.spent_hours <= 0:
-        return Response('Wrong spent_hours, it must be greater than 0', status_code=status.HTTP_400_BAD_REQUEST)
-
-    task_manager.add_task(data.task_name, data.progress, data.spent_hours)
+    try:
+        task_manager.add_task(data.task_name, data.progress, data.spent_hours)
+    except Exception as e:
+        return Response(f'Raised exception {e}', status_code=status.HTTP_400_BAD_REQUEST)
     return Response('Task successfully added', status_code=status.HTTP_200_OK)
